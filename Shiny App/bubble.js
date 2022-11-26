4e6942cd473cc2de100ba823942584a2845e250f
Adacko2911 <a.dacko@students.uu.nl>
2022-11-26 01:58:13 +0100

    updating subject category colors

--- Shiny App/bubble.js
+++ Shiny App/bubble.js
@@ -17,7 +17,10 @@ var pack = d3.pack()
   .padding(15);
     
 var format = d3.format(",d");
-var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+var colors = new Map([["Study design", "black"], ["Statistical models", "white"], ["Epidemiology", "blue"],["Data Collection methods", "yellow"],["Specific Data Analysis","green"]])
+
+//var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
 var group = svg.append("g");
 
@@ -50,7 +53,12 @@ r2d3.onRender(function(data, svg, width, height, options) {
         d.id = id;
       }
     });
+  var dacol = ["Strong theoretical base of data science practice and ethics",	"Causal inference",	"Data Collection methods",	"Database Management",	"nonSQL databases",	"Relational databases",	"Data Imputation"	,"Supervised Machine Learning","Data wrangling",	"Deep learning",	"Network science",	"Unsupervised Machine Learning",	"Programming", 	"SQL",	"Python", 	"Data clean-up",	"QGIS",	"PCRaster",	"R	Rmardown",	"Epidemiology",	"Bayesian statistics",	"Statistical models",	"Stochastic Modeling",	"Explorative Data Analysis",	"Specific Data Analysis",	"Simulation models",	"PAC",	"Data mining",	"Big data",	"Visualizations",	"Graph Analysis/Graph construction",	"Data manipulation",	"SPSS",	"Complex systems",	"MAL",	"HLM",	"Text mining",	"Study design",	"Stata",	"JASP",	"Haskell",	"Mplus","Visual Studio"]
+  var myColor = d3.scaleOrdinal().domain(dacol)
+  .range(["#48bf8e", "#48bf8e", "#2c4e2f", "#1f3e9e", "#78ee5a", "#9c1a54", "#a3c541", "#6f1996", "#d595d9", "#573f56", "#eb67f9", "#6f85a6", "#77d1fd", "#21a708", "#9a2a06", "#f7b8a2", "#683d0d", "#fe8f06", "#fd2c3b", "#cdd9b8", "#2580fe", "#7212ff", "#ffce54", "#ff0087", "#a27c59","#69ef7b", "#cb1775", "#528f7a", "#992a13", "#d0d2f0", "#154e56", "#e78361", "#77d6cf", "#6e334f", "#afd35a", "#b97eac", "#f0d27e", "#f231fc", "#5b8313", "#6146ca", "#b28b28", "#ee80fe", "#19a71f", "#0494fb", "#fc2c44", "#6108e8", "#55450a", "#1a4fa3"])
+ //var color = d3.scaleOrdinal(d3.schemeCategory20c)
 
+  
   var node = group.selectAll(".node")
     .data(pack(root).leaves())
     .enter().append("g")
@@ -65,7 +73,8 @@ r2d3.onRender(function(data, svg, width, height, options) {
       .duration(400)
       .ease(d3.easeExp)
       .attr("r", function(d) { return d.r; })
-      .style("fill", function(d) { return color(d.id); });
+      .attr("fill", function(d){return myColor(d.id) });
+     // .style("fill", function(d) { return color(d.id); });
   node.on("click", function(d) {
         var message = d.id;
         Shiny.onInputChange("click_event",message)
